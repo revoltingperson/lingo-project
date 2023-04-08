9e8b3df30f42c18c87b9a54eb64a3ad7ddb2cc08
Vadim <vadimsphere@gmail.com>
2023-04-08 22:04:42 +0500

    before changing api to freedictionary

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ import linecache
 import os
 import random
 import requests
+import uvicorn
 
 TOTAL_WORDS = 233464
 YANDEX_URL = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key="
@@ -27,15 +28,24 @@ def prepare_word(line_in: int) -> dict:
     selected_line.strip(".,")
     if selected_line.endswith('\n'):
         selected_line = selected_line[:-1]
+    if len(selected_line) < 3:
+        return {}
     selected_line = selected_line.split()[ONE_WORD]
-    return req_to_yandex(selected_line)
-
-
-def word_matches_pos(target_pos: str, definitions: dict) -> bool:
-    for obj in definitions['tr']:
-        if obj['pos'] == target_pos:
-            return True
-    return False
+    pack = req_to_yandex(selected_line)
+    if not pack or pack.get('def') is None:
+        return {}
+    new_obj = {'word': None, 'pos': []}
+    if pack['def']:
+        new_obj['word'] = pack['def'][ONE_WORD]['text']
+        for obj in pack['def']:
+            for sub_obj in obj['tr']:
+                if sub_obj['pos'] not in new_obj['pos']:
+                    new_obj['pos'].append(sub_obj['pos'])
+        if 'participle' in new_obj['pos']:
+            new_obj['pos'].remove('participle')
+            new_obj['pos'].extend(('verb', 'adjective'))
+            new_obj['pos'] = list(set(new_obj['pos']))
+    return new_obj
 
 
 def generate_exercise(word_count: int) -> list:
@@ -45,31 +55,32 @@ def generate_exercise(word_count: int) -> list:
     word_set = [target_pos]
     at_least_one_correct = False
     for ar_in, word_ind in enumerate(word_indexes):
-        continue_search = True
-        while continue_search:
+        one_word_obj: dict = {}
+        while not one_word_obj:
             one_word_obj = prepare_word(word_ind)
-            if not one_word_obj or one_word_obj.get('def') is None:
+            if not one_word_obj:
                 return []
-            if one_word_obj['def']:
-                matched = word_matches_pos(target_pos, one_word_obj['def'][ONE_WORD])
+            if one_word_obj['word'] is None:
+                one_word_obj.clear()
+            else:
+                matched = target_pos in one_word_obj['pos']
                 if matched:
                     at_least_one_correct = True
                 if ar_in == word_count - 1 and not at_least_one_correct:
-                    pass
+                    one_word_obj.clear()
                 else:
-                    word_set.append({'word': one_word_obj['def'][ONE_WORD]['text'],
-                                     'pos': target_pos if matched else one_word_obj['def'][ONE_WORD]['pos']})
-                    continue_search = False
+                    word_set.append(one_word_obj)
                     continue
             word_ind = word_ind + 1 if word_ind < TOTAL_WORDS else 1
     return word_set
 
 
-@app.get("/get-new-task/")
-async def root(word_count: int = 7):
+@app.get("/get-new-tasks/")
+async def task_generator(word_count: int = 7):
     if word_count > 10:
         word_count = 10
     res_payload = generate_exercise(word_count)
     if not res_payload:
         return {"error_msg": "internal api problem"}
+    print(res_payload)
     return {"words": res_payload}
